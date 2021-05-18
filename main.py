bac2e6f849076e878bb5ce54896643a52da5a256
Jason Aik <jasonaik39@gmail.com>
2021-05-18 18:58:09 +0800

    Add environnment variables

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from typing import Callable
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///cafes.db")
