bd82f226791c0a604158446ad3b3f41735f3ebdf
Jason Aik <jasonaik39@gmail.com>
2021-05-18 19:04:59 +0800

    Delete unnecessary comment

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///cafes.db")
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
